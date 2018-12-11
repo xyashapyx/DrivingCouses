db736dc7479617ac79bb093c3765250096f24347
Pavlo Vorontsov <pvo@smartbusiness.com.ua>
2018-12-11 22:42:00 +0200

    Add project files.

---
+++ DrivingCourses.Core/ViewModels/PageViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross.Core.ViewModels;
+
+namespace DrivingCourses.Core.ViewModels.PageViewModels
+{
+    public class MainViewModel : MvxViewModel
+    {
+    }
+}
---
+++ DrivingCouses/DrivingCouses/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DrivingCourses.Core.ViewModels.PageViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using DrivingCouses.Views;
+using MvvmCross.IoC;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace DrivingCouses
+{
+    public partial class App : MvvmCross.Core.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<MainViewModel>();
+            // if you want to use a custom AppStart, you should replace the previous line with this one:
+            // RegisterCustomAppStart<MyCustomAppStart>();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
